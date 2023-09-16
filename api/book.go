df0b20e43d28281e22829793035a26ffce254940
PetrusAriaa <petrusariacr25@gmail.com>
2023-09-16 07:32:52 +0700

    feat: create placeholder for other APIs

--- api/book.go
+++ api/book.go
@@ -8,6 +8,9 @@ import (
 
 func BookRouter(prefix string, r *mux.Router) {
 	b := r.PathPrefix(prefix).Subrouter()
-	b.HandleFunc("/", controller.GetBook)
-	b.HandleFunc("/{id}", controller.GetBookById)
+	b.HandleFunc("/", controller.GetBook).Methods("GET")
+	b.HandleFunc("/{id}", controller.GetBookById).Methods("GET")
+	b.HandleFunc("/", controller.AddBook).Methods("POST")
+	b.HandleFunc("/{id}", controller.UpdateBook).Methods("PUT")
+	b.HandleFunc("/{id}", controller.DeleteBook).Methods("DELETE")
 }
