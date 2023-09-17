413ad25f17ade9d20e32590ac938545a02925b9a
PetrusAriaa <petrusariacr25@gmail.com>
2023-09-17 16:32:14 +0700

    fix: update book model

--- models/book.go
+++ models/book.go
@@ -3,9 +3,16 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Book struct {
-	Id     primitive.ObjectID `bson:"_id"`
-	Title  string
-	Author string
-	Page   int
-	Rate   int
+	Id       primitive.ObjectID `bson:"_id"`
+	Metadata Metadata           `json:"metadata"`
+	Rate     int                `json:"rate"`
+	Price    int                `json:"price"`
+	Stocks   int                `json:"stocks"`
+}
+
+type Metadata struct {
+	ISBN   string `json:"ISBN"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Page   int    `json:"page"`
 }
