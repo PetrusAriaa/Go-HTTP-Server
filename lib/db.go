a09cb5dc494400a02916f68a9f525468ca2182b9
PetrusAriaa <petrusariacr25@gmail.com>
2023-09-23 21:57:43 +0700

    feat: create connection to mongoDB

---
+++ lib/db.go
@@ -0,0 +1,26 @@
+package lib
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading environment: %v", err)
+	}
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opt := options.Client().ApplyURI(os.Getenv("MONGODB_URI")).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(context.Background(), opt)
+
+	if err != nil {
+		log.Fatalf("Error connecting MongoDB: %v", err)
+	}
+	return client
+}
