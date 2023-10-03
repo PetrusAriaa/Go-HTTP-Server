0dde7e5eb7a699589a684b6a03f5b5f666dd9153
PetrusAriaa <petrusariacr25@gmail.com>
2023-10-03 18:16:16 +0700

    feat: create logger to properly log activities

--- controller/book.controller.go
+++ controller/book.controller.go
@@ -4,16 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
-	"log"
 	"net/http"
 	"time"
 
 	"github.com/PetrusAriaa/go-http-server/lib"
+	"github.com/PetrusAriaa/go-http-server/logger"
 	"github.com/PetrusAriaa/go-http-server/models"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type BookList struct {
@@ -34,20 +34,25 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 
 	conn := lib.ConnectDB()
 	coll := conn.Database("bookstore").Collection("book")
-
 	defer conn.Disconnect(context.Background())
+
 	c, err := coll.Find(context.Background(), bson.D{})
 	if err != nil {
-		panic(err)
+		logger.ResponseLogger(r, http.StatusInternalServerError, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	if err = c.All(context.Background(), &Books); err != nil {
-		panic(err)
+		logger.ResponseLogger(r, http.StatusInternalServerError, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(BookList{Data: Books, Length: int64(len(Books)), Accessed: time.Now()})
+	logger.ResponseLogger(r, http.StatusOK, "Response sent")
 }
 
 func GetBookById(w http.ResponseWriter, r *http.Request) {
@@ -56,40 +61,48 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 
 	conn := lib.ConnectDB()
 	coll := conn.Database("bookstore").Collection("book")
+	defer conn.Disconnect(context.Background())
+
 	param := mux.Vars(r)["id"]
 	_id, err := primitive.ObjectIDFromHex(param)
 	if err != nil {
-		panic(err)
+		logger.ResponseLogger(r, http.StatusBadRequest, err.Error())
+		http.Error(w, "server: bad request", http.StatusBadRequest)
+		return
 	}
-
-	defer conn.Disconnect(context.Background())
 
 	filter := bson.D{{Key: "_id", Value: _id}}
 	if err := coll.FindOne(context.Background(), filter).Decode(&Book); err != nil {
-		log.Println(err)
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
+		if err == mongo.ErrNoDocuments {
+			logger.ResponseLogger(r, http.StatusNotFound, err.Error())
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 	}
+
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(SingleBook{Data: Book, Length: 1, Accessed: time.Now()})
+	logger.ResponseLogger(r, http.StatusOK, "Response sent")
 }
 
 func AddBook(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
 	var Book models.Book
 
 	if err := json.NewDecoder(r.Body).Decode(&Book); err != nil {
-		log.Println(err.Error())
+		logger.ResponseLogger(r, http.StatusInternalServerError, err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer r.Body.Close()
 
 	conn := lib.ConnectDB()
 	coll := conn.Database("bookstore").Collection("book")
 	defer conn.Disconnect(context.Background())
 
 	if err := validateBookRequest(&Book); err != nil {
+		logger.ResponseLogger(r, http.StatusBadRequest, err.Error())
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -97,13 +110,15 @@ func AddBook(w http.ResponseWriter, r *http.Request) {
 	Book.Id = primitive.NewObjectID()
 	_, err := coll.InsertOne(context.Background(), Book)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Internal Server Error %v", err.Error()), http.StatusInternalServerError)
+		logger.ResponseLogger(r, http.StatusInternalServerError, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(201)
 	json.NewEncoder(w).Encode(map[string]string{"message": "object created"})
+	logger.ResponseLogger(r, http.StatusCreated, "Document created successfully")
 }
 
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
@@ -119,28 +134,31 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	param := mux.Vars(r)["id"]
 	_id, err := primitive.ObjectIDFromHex(param)
 	if err != nil {
-		panic(err)
+		logger.ResponseLogger(r, http.StatusBadRequest, err.Error())
+		http.Error(w, "server: bad request", http.StatusBadRequest)
+		return
 	}
 
 	res, err := coll.DeleteOne(context.Background(), bson.D{{Key: "_id", Value: _id}})
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		logger.ResponseLogger(r, http.StatusInternalServerError, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if res.DeletedCount == 0 {
-		log.Printf("Cannot find document with id: %s", param)
-		http.Error(w, "book not found", http.StatusNotFound)
+		logger.ResponseLogger(r, http.StatusNotFound, "mongo: no documents to delete")
+		http.Error(w, "mongo: no documents to delete", http.StatusNotFound)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
+	logger.ResponseLogger(r, http.StatusNoContent, "Document deleted successfully")
 }
 
 func validateBookRequest(book *models.Book) error {
 	if book.Metadata.Author == "" || book.Metadata.ISBN == "" || book.Metadata.Title == "" || book.Metadata.Page == 0 {
-		return errors.New("missing required fields")
+		return errors.New("server: missing required fields")
 	}
 	return nil
 }
